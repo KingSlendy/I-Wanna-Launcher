7076b7334b14abff660079febdcf936c9ff6335a
KingSlendy <kingslendyCSP@outlook.com>
2025-10-11 01:13:09 -0700

    Included Python3.8.10 as embeddable, moved launcher.py to this Python38 folder and added the option to generate the .exe with a .ico

--- generator.py
+++ generator.py
@@ -1,5 +1,9 @@
 import os, shutil, subprocess
 
+PYTHON_FOLDER = "Python38"
+PYTHON_PATH = f"{PYTHON_FOLDER}\\python.exe"
+GENERATED_FOLDER = "Generated"
+
 def main():
     print("Welcome to I Wanna Launcher!")
     name = input("What is the name of your fangame?: ")
@@ -13,8 +17,15 @@ def main():
     name_short = "".join([x[0] for x in name.split(" ")])
     releases = releases if releases in ("y", "n") else "y"
     releases = "-Releases" if releases == "y" else ""
+    icon = ""
 
-    with open("launcher.py", "r") as file:
+    if os.path.exists(f"{GENERATED_FOLDER}\\icon.ico"):
+        icon = f"--icon={GENERATED_FOLDER}\\icon.ico"
+
+    if os.path.exists(f"{GENERATED_FOLDER}\\{name_full}.exe"):
+        os.remove(f"{GENERATED_FOLDER}\\{name_full}.exe")
+
+    with open(f"{PYTHON_FOLDER}\\launcher.py", "r") as file:
         data = file.read()
 
     data = data.replace("@@USERNAME@@", username)
@@ -27,18 +38,18 @@ def main():
     with open("temp-launcher.py", "w") as file:
         file.write(data)
 
-    libraries = ["requests", "tqdm", "pywin32", "pyinstaller"]
+    #libraries = ["requests", "tqdm", "pywin32", "pyinstaller"]
 
-    for library in libraries:
-        subprocess.run(f"pip install {library}")
+    #for library in libraries:
+    #    subprocess.run(f"{PYTHON_PATH} -m pip install {library}")
 
-    subprocess.run(f"pyinstaller -F temp-launcher.py")
+    subprocess.run(f"{PYTHON_PATH} -m PyInstaller -F {icon} temp-launcher.py")
     os.remove("temp-launcher.py")
     shutil.rmtree("build")
     os.remove("temp-launcher.spec")
-    shutil.copyfile("dist\\temp-launcher.exe", "temp-launcher.exe")
-    os.rename("temp-launcher.exe", f"{name_full}.exe")
+    shutil.copyfile("dist\\temp-launcher.exe", f"{GENERATED_FOLDER}\\temp-launcher.exe")
     shutil.rmtree("dist")
+    os.rename(f"{GENERATED_FOLDER}\\temp-launcher.exe", f"{GENERATED_FOLDER}\\{name_full}.exe")
 
     
 if __name__ == "__main__":
